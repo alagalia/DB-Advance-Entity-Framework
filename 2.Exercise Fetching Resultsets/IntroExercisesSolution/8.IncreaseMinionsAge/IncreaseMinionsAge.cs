8ad8e3e366cf4008270f82b636d2aa6892ba8ac4
Галя Георгиева <alice.gospodinova@gmail.com>
2016-10-25 16:34:26 +0300

    Increase age query

---
+++ 2.Exercise Fetching Resultsets/IntroExercisesSolution/8.IncreaseMinionsAge/IncreaseMinionsAge.cs
@@ -0,0 +1,47 @@
+﻿namespace _8.IncreaseMinionsAge
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    internal class IncreaseMinionsAge
+    {
+        private static void Main()
+        {
+            SqlConnection connection =
+                new SqlConnection("Server=(localdb)\\MSSQLLocalDB; Database=MinionsDB; Trusted_Connection=True");
+
+            List<int> ids = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            using (connection)
+            {
+                connection.Open();
+                foreach (int id in ids)
+                {
+                    SqlCommand cmd = new SqlCommand("UPDATE Minions SET Age += 1 WHERE ID = @id", connection);
+                    SqlCommand cmdTitleCase =
+                        new SqlCommand(
+                            "UPDATE Minions SET Name = UPPER(LEFT(Name,1))+LOWER(SUBSTRING(Name,2,LEN(Name))) WHERE ID = @id",
+                            connection);
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmdTitleCase.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                    cmdTitleCase.ExecuteNonQuery();
+                }
+
+                SqlCommand selectCommand = new SqlCommand("SELECT * FROM Minions", connection);
+                SqlDataReader minionsReader = selectCommand.ExecuteReader();
+                while (minionsReader.Read())
+                {
+                    for (int i = 0; i < minionsReader.FieldCount; i++)
+                    {
+                        Console.Write($"{minionsReader[i]} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
