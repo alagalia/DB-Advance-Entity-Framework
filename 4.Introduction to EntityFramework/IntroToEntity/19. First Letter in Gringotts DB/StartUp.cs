37b99e1994fcc11a79de8f4982e9ed428499796c
Галя Георгиева <alice.gospodinova@gmail.com>
2016-11-06 12:39:38 +0200

    exercise 19

---
+++ 4.Introduction to EntityFramework/IntroToEntity/19. First Letter in Gringotts DB/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.First_Letter_in_Gringotts_DB
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            using (GringottsContext context = new GringottsContext())
+            {
+                var wiz = context.WizzardDeposits.Where(w => w.DepositGroup == "Troll Chest").Select(w=>w.FirstName).Distinct();
+                foreach (var w in wiz)
+                {
+                    Console.WriteLine(w[0]);
+                }
+            }
+        }
+    }
+}
