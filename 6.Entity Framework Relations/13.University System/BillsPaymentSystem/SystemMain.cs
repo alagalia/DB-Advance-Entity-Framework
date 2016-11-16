508349803a805bedd4355df1a62aab1b7cbed699
Галя Георгиева <alice.gospodinova@gmail.com>
2016-11-16 20:34:16 +0200

    fix name

---
+++ 6.Entity Framework Relations/13.University System/BillsPaymentSystem/SystemMain.cs
@@ -0,0 +1,21 @@
+﻿namespace BillsPaymentSystem
+{
+    class SystemMain
+    {
+        static void Main()
+        {
+            //----------create only class People in DB. !!! Nothing in modelBuilder.
+            TPHuniversityContext tphContext = new TPHuniversityContext();
+            tphContext.Database.Initialize(true);
+
+
+            //--------------create separated tables. One for Student and one for Teacher
+            TpTuniversityContext tptContext = new TpTuniversityContext();
+            tptContext.Database.Initialize(true);
+
+            //-----------Without inheritance. Put all props into the entity.
+            TPCuniversityContext tpcContext = new TPCuniversityContext();
+            tpcContext.Database.Initialize(true);
+        }
+    }
+}
