dab307af23b6f109ad9b35996cb3491591b9a7d4
Галя Георгиева <alice.gospodinova@gmail.com>
2016-11-26 13:05:05 +0200

    reader from file

---
+++ DataReader from file/ByteArrayManipulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace PhotoShare.Client
+{
+    public class ByteArrayManipulator
+    {
+        public static void Main()
+        {
+            byte[] bytes;
+            using (StreamReader reader = new StreamReader("test.txt"))
+            {
+
+                using (var memstream = new MemoryStream())
+                {
+                    reader.BaseStream.CopyTo(memstream);
+                    bytes = memstream.ToArray();
+                }
+            }
+
+            string result = System.Text.Encoding.UTF8.GetString(bytes);
+            File.WriteAllBytes("testResult.txt", bytes);
+            Console.WriteLine(result);
+        }
+    }
+}
